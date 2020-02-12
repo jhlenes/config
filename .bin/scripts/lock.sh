c566170b0f1f296e5ea2338ea6b498c65a54a8e6
Henrik Lenes <henrik@maritimerobotics.com>
2020-02-12 08:56:28 +0100

    More stuff

--- .bin/scripts/lock.sh
+++ .bin/scripts/lock.sh
@@ -47,4 +47,4 @@ fi
 
 # Stop Spotify
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -e -n -f -i /tmp/screen.png
+i3lock -e -f -i /tmp/screen.png
